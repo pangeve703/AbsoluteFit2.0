e8838da7399a9e48b476cea6e57d789116c67090
pangeve703 <49011770+pangeve703@users.noreply.github.com>
2020-01-14 02:18:33 +0800

    init

--- app/src/main/java/com/example/absolutefit/CalculateBMI.kt
+++ app/src/main/java/com/example/absolutefit/CalculateBMI.kt
@@ -1,13 +1,83 @@
 package com.example.absolutefit
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.Menu
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_calculate_bmi.*
+import java.lang.Math.pow
+import kotlin.math.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import java.lang.Exception
+
 
 class CalculateBMI : AppCompatActivity() {
 
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculate_bmi)
+        val toolbar: Toolbar = findViewById(R.id.toolbar1)
+        setSupportActionBar(toolbar)
+        val reportBtn : Button = findViewById(R.id.report_btn) as Button
+        reportBtn.setOnClickListener(){
+            Toast.makeText(this@CalculateBMI, "TESTING BUTTON CLICK 2", Toast.LENGTH_SHORT).show()
+            val intentGoToProgressReport = Intent(this,ProgressReport::class.java)
+            startActivity(intentGoToProgressReport)
+        }
+
+        val calBMIBtn:Button = findViewById(R.id.calBMI_btn) as Button
+        calBMIBtn.setOnClickListener(){
+            try {
+
+                val height:Double = (height_input.text.toString().toDouble())/100
+                val weight:Double = weight_input.text.toString().toDouble()
+                val result:Double =weight/ (height * height )
+
+                if(result<=25&&result>=20){
+                    result_txt_view.setTextColor(getColor(R.color.green))
+                }
+                else if (result<=30&&result>=25){
+                    result_txt_view.setTextColor(getColor(R.color.yellow))
+                }
+                else if(result<=40&&result>=30){
+                    result_txt_view.setTextColor(getColor(R.color.orange))
+                }
+                else
+                    result_txt_view.setTextColor(getColor(R.color.red))
+
+                result_txt_view.setText("%.2f".format(result))
+            } catch (e: Exception) {
+                Toast.makeText(this@CalculateBMI, "Please enter your height,weight and age", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val resetBtn:Button = findViewById(R.id.resetBMI_btn) as Button
+        resetBtn.setOnClickListener(){
+            height_input.setText("")
+            weight_input.setText("")
+            age_input.setText("")
+            result_txt_view.setText("")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main2, menu)
+        return true
     }
 
 }
--- app/src/main/java/com/example/absolutefit/MainActivity.kt
+++ app/src/main/java/com/example/absolutefit/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.absolutefit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar1)
+        setSupportActionBar(toolbar)
+
 
         val bmiButton: ImageButton = findViewById(R.id.calbmi_btn) as ImageButton
         bmiButton.setOnClickListener {
@@ -18,7 +26,18 @@ class MainActivity : AppCompatActivity() {
             val intentGoToCalculateBMI = Intent(this,CalculateBMI::class.java)
             startActivity(intentGoToCalculateBMI)
         }
+
+        val signUpButton: ImageButton = findViewById(R.id.signUp_btn) as ImageButton
+        signUpButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "TESTING BUTTON CLICK 1", Toast.LENGTH_SHORT).show()
+            val intentGoToMain2Activity = Intent(this,Main2Activity::class.java)
+            startActivity(intentGoToMain2Activity)
+        }
     }
 
-
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main2, menu)
+        return true
+    }
 }
